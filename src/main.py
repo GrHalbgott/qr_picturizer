e1762eac8d478396420435ae67a8f0e518928b47
GrHalbgott <gr.halbgott53@web.de>
2023-11-08 00:30:02 +0100

    feat/working replacer, only grayscale

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ from PIL import Image
 
 from modules.utils import _check_input_arguments, init_logger
 from modules.qr_generator import qr_generator
-from modules.operator import read_raster, read_image_list, raster_enlarger
+from modules.operator import read_raster, read_image_list, raster_enlarger, replacer
 
 
 if __name__ == "__main__":
@@ -25,6 +25,7 @@ if __name__ == "__main__":
     path_qr = "./results/qr_code.png"
     outfile = "./results/qr_code_picturized.png"
 
+    logging.info("Checking inputs...")
     input, check = _check_input_arguments()
 
     if check:
@@ -39,14 +40,23 @@ if __name__ == "__main__":
     assert infolder.exists(), "No images found in data/images. Please add images to this folder and rerun the program."
 
     # Create image list
+    logging.info("Creating image list...")
     img_list = read_image_list(infolder)
 
     # Read random image from list
     img = read_raster(Path(infolder / np.random.choice(img_list)))
+    logging.info(f"Random image shape: {img.shape}")
 
     # Enlarge QR code
+    logging.info("Enlarging QR code...")
     data = raster_enlarger(outfolder, img, data)
     logging.info(f"Enlarged array shape: {data.shape}")
 
+    logging.info("Replacing QR code with images...")
+    data = replacer(infolder, img, data, img_list)
+
+    np.savetxt(outfolder / "qr_picturized.txt", data)
+
+    logging.info("Saving image...")
     image = Image.fromarray(data)
     image.save(outfile)
--- src/modules/operator.py
+++ src/modules/operator.py
@@ -6,6 +6,7 @@
 import os
 import numpy as np
 import rasterio as rio
+from pathlib import Path
 from PIL import Image
 
 
@@ -32,7 +33,7 @@ def read_image_list(path):
     return img_list
 
 
-def raster_enlarger(outfolder, img, data):
+def raster_enlarger(img, data):
     """
     Expand the QR code using the shape of the input image
     :param data: QR code as Numpy array
@@ -41,7 +42,27 @@ def raster_enlarger(outfolder, img, data):
     data_ex = data.repeat(img.shape[0], axis=0).repeat(img.shape[1], axis=1)
     data_ex = np.multiply(data_ex, 255).astype(np.uint8)
 
-    image = Image.fromarray(data_ex)
-    image.save(outfolder / "qr_expanded.png")
-
     return data_ex
+
+
+def replacer(infolder, img, data, img_list):
+    """
+    Iterates through array and replaces blocks with zeros with images
+    :param infolder: path to images
+    :param img: input image
+    :param data: QR code as Numpy array
+    :param img_list: list of images
+    :return: updated Numpy array
+    """
+    shape_x = data.shape[0]
+    shape_y = data.shape[1]
+
+    for j in range(0, shape_y, img.shape[1]):
+        for i in range(0, shape_x, img.shape[0]):
+            img = read_raster(Path(infolder / np.random.choice(img_list)))
+            if data[i, j] == 0:
+                data[i:i + img.shape[0], j:j + img.shape[1]] = img
+            else:
+                data[i:i + img.shape[0], j:j + img.shape[1]] = 255
+
+    return data
